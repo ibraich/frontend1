d884b9d42159d87aa888fcf29d34016d5a36d1dd
ibrahim erdogan <ibo9819@gmail.com>
2022-07-11 13:10:34 +0300

    initial header

--- images-gallery-frontend/src/App.js
+++ images-gallery-frontend/src/App.js
@@ -1,8 +1,9 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
 function App() {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery" />
     </div>
   );
 }
