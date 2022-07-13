125a03f0851ec47b28df7efd95a84294ab451b98
ibrahim erdogan <ibo9819@gmail.com>
2022-07-13 12:11:23 +0300

    fixed code with linter

--- images-gallery-frontend/src/Components/Search.js
+++ images-gallery-frontend/src/Components/Search.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { Row, Container, Col, Button, Form } from "react-bootstrap";
 
-const Search = ({word,setWord,handleSubmit}) => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Form onSubmit={handleSubmit}>
-        <Row className="justify-content-center" >
+        <Row className="justify-content-center">
           <Col xs={12} md={8}>
-            <Form.Control 
-            type="text"
-            value={word}
-            onChange={(e)=>setWord(e.target.value)}
-            placeholder="Search Image Name" 
-
+            <Form.Control
+              type="text"
+              value={word}
+              onChange={(e) => setWord(e.target.value)}
+              placeholder="Search Image Name"
             />
           </Col>
-          <Col >
+          <Col>
             <Button variant="primary" type="submit">
               Search
             </Button>
