1554e6ba078dce60bc000ae1fea89f058784d60c
ibrahim erdogan <ibo9819@gmail.com>
2022-07-13 11:17:32 +0300

    clear search upon submit

--- images-gallery-frontend/src/App.js
+++ images-gallery-frontend/src/App.js
@@ -2,15 +2,26 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header";
 import Search from "./Components/Search";
-const Unsplas_Key=process.env.REACT_APP_UNSPLASH_KEY
+const Unsplash_Key=process.env.REACT_APP_UNSPLASH_KEY
 const App = () => {
   const [word, setWord] = useState("");
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${process.env.REACT_APP_UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    setWord('');
     
   };
   
   return (
+
     <div className="App">
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
