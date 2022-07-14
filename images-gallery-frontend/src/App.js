7853a1d811cabae1e7664dcdffc0a827b34e80d5
ibrahim erdogan <ibo9819@gmail.com>
2022-07-14 10:15:44 +0300

    setImages

--- images-gallery-frontend/src/App.js
+++ images-gallery-frontend/src/App.js
@@ -5,6 +5,8 @@ import Search from "./Components/Search";
 const Unsplash_Key = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -13,7 +15,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
