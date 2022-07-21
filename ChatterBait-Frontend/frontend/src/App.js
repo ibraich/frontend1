b9a18416822915d2e63b363c81609feeea5fdd42
ibrahim erdogan <ibo9819@gmail.com>
2022-07-21 14:45:57 +0300

    Creating the chatterbait app

---
+++ ChatterBait-Frontend/frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+     <h1>
+      Chattter Bait
+     </h1>
+    </div>
+  );
+}
+
+export default App;
